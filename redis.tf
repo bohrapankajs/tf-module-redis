446249f5acfdc29b1b62249d7aa6732697d682c4
Pankajbohra <bohrapankajs@gmail.com>
2023-08-14 17:45:36 +0530

    test

--- redis.tf
+++ redis.tf
@@ -1,11 +1,11 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "roboshop-redis-${var.ENV}"
   engine               = "redis"
-  node_type            = "cache.t3.micro"
-  num_cache_nodes      = 1 
+  node_type            = var.REDIS_INSTANCE_CLASS
+  num_cache_nodes      = var.REDIS_NODE_COUNT
   parameter_group_name = aws_elasticache_parameter_group.pg.name
-  engine_version       = "6.x"
-  port                 = 6379
+  engine_version       = var.REDIS_ENGINE_VERSION
+  port                 = var.REDIS_POR
   subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
   security_group_ids   = [aws_security_group.allows_redis.id]
 }
--- sg.tf
+++ sg.tf
@@ -6,16 +6,16 @@ resource "aws_security_group" "allows_redis" {
 
   ingress {
     description      = "Allow redis from default VPC"
-    from_port        = 6379
-    to_port          = 6379
+    from_port        = var.REDIS_PORT
+    to_port          = var.REDIS_PORT
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
 
   ingress {
     description      = "Allow redis from default VPC"
-    from_port        = 6379
-    to_port          = 6379
+    from_port        = var.REDIS_PORT
+    to_port          = var.REDIS_PORT
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
